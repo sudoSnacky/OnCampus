46f7f0b576534734f416cfb0e8d0362b76fb5dd8
Snacky <gamingsnacky@gmail.com>
2025-10-22 14:15:38 +0000

    name should be in google colors

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,9 +4,17 @@ export const Icons = {
   logo: (props: SVGProps<SVGSVGElement>) => (
     <span className="font-headline font-bold text-2xl" {...props}>
       <span style={{ color: '#4285F4' }}>&lt;</span>
-      <span style={{ color: '#DB4437' }}> O</span>
+      <span> </span>
+      <span style={{ color: '#DB4437' }}>O</span>
       <span style={{ color: '#F4B400' }}>N</span>
-      <span> CAMPUS </span>
+      <span> </span>
+      <span style={{ color: '#4285F4' }}>C</span>
+      <span style={{ color: '#0F9D58' }}>A</span>
+      <span style={{ color: '#DB4437' }}>M</span>
+      <span style={{ color: '#F4B400' }}>P</span>
+      <span style={{ color: '#4285F4' }}>U</span>
+      <span style={{ color: '#0F9D58' }}>S</span>
+      <span> </span>
       <span style={{ color: '#0F9D58' }}>&gt;</span>
     </span>
   ),
