2946ac298022b621fe577da5b48fbd55d9446497
Snacky <gamingsnacky@gmail.com>
2025-10-23 17:55:44 +0000

    remove all files that arent used or are fragments of old code that was d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { ArrowRight, Ticket, Users, Calendar } from "lucide-react";
 import { Button } from "../components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "../components/ui/card";
 import { Icons } from "../components/icons";
-import { initialEvents } from "../lib/data";
 
 
 const features = [
